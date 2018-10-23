05642d8a73c53e3781c663a0e69f371fb52e69db
septnn <septnn@163.com>
2018-10-23 15:12:50 +0800

    add brand

--- app/Admin/Controllers/SelfShoppingListController.php
+++ app/Admin/Controllers/SelfShoppingListController.php
@@ -22,16 +22,11 @@ class SelfShoppingListController extends Controller
      */
     public function index()
     {
-        $show = request()->show;
-        return Admin::content(function (Content $content) use ($show) {
+        return Admin::content(function (Content $content) {
 
             $content->header('header');
             $content->description('description');
-            if($show == 1) {
-                $content->body($this->grid());
-            } else {
-                $content->body($this->grid2());
-            }
+            $content->body($this->grid());
         });
     }
 
@@ -75,41 +70,50 @@ class SelfShoppingListController extends Controller
      */
     protected function grid()
     {
-        return Admin::grid(SelfShoppingList::class, function (Grid $grid) {
+        $show = request()->show;
 
-            // $grid->id('ID')->sortable();
-            $grid->product_name('产品');
-            $grid->brand_name('品牌');
-            $grid->purchase_channel('购买渠道');
-            $grid->num('数量');
-            $grid->price('价格');
-            // $grid->pictures('图片')->image();
+        if($show != 1) {
+            $show_func = function (Grid $grid) {
 
-            // $grid->created_at();
-            // $grid->updated_at();
-        });
-    }
+                $grid->product_name('产品');
+                $grid->brand_name('品牌');
+                $grid->purchase_channel('购买渠道');
+                $grid->num('数量');
+                $grid->price('价格');
+                return $grid;
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid2()
-    {
-        return Admin::grid(SelfShoppingList::class, function (Grid $grid) {
+            };
+        } else {
+            $show_func = function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
-            $grid->product_name('产品');
-            $grid->brand_name('品牌');
-            $grid->purchase_channel('购买渠道');
-            $grid->num('数量');
-            $grid->price('价格');
-            $grid->pictures('图片')->image();
+                $grid->id('ID')->sortable();
+                $grid->product_name('产品');
+                $grid->brand_name('品牌');
+                $grid->purchase_channel('购买渠道');
+                $grid->num('数量');
+                $grid->price('价格');
+                $grid->pictures('图片')->image();
 
-            $grid->created_at();
-            $grid->updated_at();
-        });
+                $grid->created_at();
+                $grid->updated_at();
+                return $grid;
+            };
+        }
+        $c = function (Grid $grid) use ($show_func) {
+            $grid = $show_func($grid);
+            $grid->filter(function($filter){
+                // 去掉默认的id过滤器
+                $filter->disableIdFilter();
+                $filter->where(function ($query) {
+                    $query->where('product_name', 'like', "%{$this->input}%")
+                        ->orWhere('brand_name', 'like', "%{$this->input}%");
+
+                }, '关键词');
+
+            });
+
+        };
+        return Admin::grid(SelfShoppingList::class, $c);
     }
 
     /**
@@ -123,7 +127,8 @@ class SelfShoppingListController extends Controller
 
             $form->display('id', 'ID');
             $form->text('product_name', '产品');
-            $form->text('brand_name', '品牌');
+            // $form->text('brand_name', '品牌');
+            $form->multipleSelect('brand_name', '品牌')->options($this->getBrandName());
             // $form->textkeywords('brand_name','选择品牌')->setData((SelfShoppingList::select()->get()->toArray()),'brand_name');
             $form->text('purchase_channel', '购买渠道');
             $form->number('num', '数量');
@@ -133,6 +138,29 @@ class SelfShoppingListController extends Controller
 
             $form->display('created_at', 'Created At');
             $form->display('updated_at', 'Updated At');
+        });
+    }
+
+    public function getBrandName($key = false)
+    {
+        $arr = ['十月结晶','Pigeon/贝亲','好孩子','爱得利','开丽','日康','NUK',
+        // '黄色小鸭',// '贝恩宝',// '运智贝'
+        ];
+        if ($key !== false && isset($arr[$key])) {
+            return $arr[$key];
+        }
+        return $arr;
+        # code...
+    }
+
+    public function create2()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('chart');
+            $content->description('.....');
+
+            $content->body(view('admin.self.shopping.list'));
         });
     }
 }
