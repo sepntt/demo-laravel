cbaa0a7d9538295aa0a21d4038d970ddd7c7e85a
septnn <septnn@163.com>
2018-10-23 16:54:10 +0800

    add brand config

--- config/customer/selfshoppinglist.php
+++ config/customer/selfshoppinglist.php
@@ -2,34 +2,48 @@
 
 return [
 	'brand' => [
-		[
-			'id' => 0,
-			'brand_name' => '十月结晶',
-		],
-		[
-			'id' => 1,
-			'brand_name' => 'Pigeon/贝亲',
-		],
-		[
-			'id' => 2,
-			'brand_name' => '好孩子',
-		],
-		[
-			'id' => 3,
-			'brand_name' => '爱得利',
-		],
-		[
-			'id' => 4,
-			'brand_name' => '开丽',
-		],
-		[
-			'id' => 5,
-			'brand_name' => '日康',
-		],
-		[
-			'id' => 6,
-			'brand_name' => 'NUK',
-		],
-    	// '黄色小鸭',// '贝恩宝',// '运智贝'
-	],
+		['id' => 1,'brand_name' => '十月结晶',],
+		['id' => 2,'brand_name' => '子初',],
+		['id' => 3,'brand_name' => '新贝',],
+		['id' => 4,'brand_name' => '开丽',],
+		['id' => 5,'brand_name' => '德祐',],
+		['id' => 6,'brand_name' => '全棉时代',],
+        ['id' => 7,'brand_name' => '欧思朵',],
+        ['id' => 8,'brand_name' => 'dacco三洋',],
+        ['id' => 9,'brand_name' => 'babycare',],
+        ['id' => 10,'brand_name' => '安慕斯',],
+        ['id' => 11,'brand_name' => '可优比',],
+        ['id' => 12,'brand_name' => '红色小象',],
+        ['id' => 13,'brand_name' => '棉花秘密',],
+        ['id' => 14,'brand_name' => 'fivetrucks',],
+        ['id' => 15,'brand_name' => '贝亲',],
+        ['id' => 16,'brand_name' => 'NUK',],
+        ['id' => 17,'brand_name' => '帮宝适',],
+        ['id' => 18,'brand_name' => '花王',],
+        ['id' => 19,'brand_name' => '大王',],
+        ['id' => 20,'brand_name' => '好奇',],
+        ['id' => 21,'brand_name' => '尤妮佳',],
+        ['id' => 22,'brand_name' => '良良',],
+        ['id' => 23,'brand_name' => 'spiritkids',],
+        ['id' => 24,'brand_name' => '小白熊',],
+        ['id' => 25,'brand_name' => '嫚熙',],
+        ['id' => 26,'brand_name' => '幼蓓',],
+        ['id' => 27,'brand_name' => '屁屁乐',],
+        ['id' => 28,'brand_name' => '好孩子',],
+        ['id' => 29,'brand_name' => '蒂爱',],
+        ['id' => 30,'brand_name' => '维达',],
+        ['id' => 31,'brand_name' => '可心柔',],
+        ['id' => 32,'brand_name' => '心心相印',],
+        ['id' => 33,'brand_name' => '顺清柔',],
+        ['id' => 34,'brand_name' => '五羊',],
+        ['id' => 35,'brand_name' => '雅特茂',],
+        ['id' => 36,'brand_name' => '富朵',],
+        ['id' => 37,'brand_name' => '乔妮雅',],
+        ['id' => 38,'brand_name' => 'pinkpanda',],
+        ['id' => 39,'brand_name' => '梦蜜',],
+
+
+
+
+    ],
 ];
